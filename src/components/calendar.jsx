508d02298adbf980f47da0e6825ceb965a5f26d7
sheldar@wp.pl <sheldar@wp.pl>
2016-02-14 15:19:53 +0100

    Example events

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -7,6 +7,36 @@ import { Col, Grid, Panel, Row } from 'react-bootstrap';
 BigCalendar.momentLocalizer(moment);
 
 const Calendar = React.createClass({
+    getInitialState () {
+        return {
+            events: [{
+                'title': 'I',
+                'start': new Date(2016, 1, 1, 0, 0),
+                'end': new Date(2016, 1, 1, 8, 0)
+            }, {
+                'title': 'II',
+                'start': new Date(2016, 1, 2, 8, 0),
+                'end': new Date(2016, 1, 2, 16, 0)
+            }, {
+                'title': 'III',
+                'start': new Date(2016, 1, 3, 16, 0),
+                'end': new Date(2016, 1, 3, 23, 59)
+            }, {
+                'title': 'I',
+                'start': new Date(2016, 1, 4, 0, 0),
+                'end': new Date(2016, 1, 4, 8, 0)
+            }, {
+                'title': 'II',
+                'start': new Date(2016, 1, 5, 8, 0),
+                'end': new Date(2016, 1, 5, 16, 0)
+            }, {
+                'title': 'III',
+                'start': new Date(2016, 1, 6, 16, 0),
+                'end': new Date(2016, 1, 6, 23, 59)
+            }]
+        };
+    },
+
     render () {
         return (
             <Grid>
@@ -14,9 +44,7 @@ const Calendar = React.createClass({
                     <Col xs={12} xsOffset={1}>
                         <Panel header="Calendar" >
                             <BigCalendar
-                                endAccessor="endDate"
-                                events={[]}
-                                startAccessor="startDate"
+                                events={this.state.events}
                             />
                         </Panel>
                     </Col>
