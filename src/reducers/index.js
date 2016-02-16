076825c9fef356df87442ace6a31c500fe7052e9
sheldar@wp.pl <sheldar@wp.pl>
2016-02-16 23:09:35 +0100

    Removed no longer used files

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const redux = require('redux');
-const modals = require('./modals');
-const shifts = require('./shifts');
-const workers = require('./workers');
-
-module.exports = redux.combineReducers({
-    modals, shifts, workers
-});
